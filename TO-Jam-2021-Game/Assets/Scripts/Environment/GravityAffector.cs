c70f9942f1f08c797456b735e5f93e6702e22d0a
josephballantyne <josephballantyne@outlook.com>
2021-05-14 16:48:23 -0400

    // bunch o stuff

---
+++ TO-Jam-2021-Game/Assets/Scripts/Environment/GravityAffector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityAffector : MonoBehaviour
+{
+    public float _slowMultiplier = 1.0f;
+
+    // toggles whether or not this platform is affected by gravity
+    public bool _affectedByGravity;
+
+    public Rigidbody2D _rigidBody;
+    // scales the action speed of everything done by this transformer
+    public float _gravityBaseScaleValue = 1.0f;
+
+    private void FixedUpdate()
+    {
+        _rigidBody.velocity = new Vector2();
+    }
+
+    private void Update()
+    {
+        // convert the bool to an int
+        int abgInt = _affectedByGravity ? 1 : 0;
+
+        // update this objects gravity scalar to be equal to its base multiplier times the slow multiplier
+         _rigidBody.gravityScale = (_gravityBaseScaleValue * _slowMultiplier) * abgInt;
+    }
+
+    // updates the value of the slow multiplier
+    public void UpdateSlowMulti(float multiplier)
+    {
+        _slowMultiplier = multiplier;
+    }
+}
---
+++ TO-Jam-2021-Game/Assets/Scripts/Tools/ColliderFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderFollow : MonoBehaviour
+{
+    public GameObject attachedRigidbody;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // ensure that this collider is identical to the attached collider
+        transform.position = attachedRigidbody.transform.position;
+        transform.rotation = attachedRigidbody.transform.rotation;
+        transform.localScale = attachedRigidbody.transform.localScale;
+    }
+}
