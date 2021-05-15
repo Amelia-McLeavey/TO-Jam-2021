4ec8c61246230b0685ce722975f055d973c1e669
Zoeghs <pilgrimz@sheridancollege.ca>
2021-05-15 18:36:11 -0400

    Set up seperate volume sliders & started on pause menu
    - Sliders are set up for background music and sound effects volume.
    - Canvases are set up to be hooked up to pause menu.

---
+++ TO-Jam-2021-Game/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Var for pausing via button click
+    private bool pauseClicked = false;
+
+    // Bool for pausing / unpausing
+    private bool gamePaused = false;
+
+    private void Update()
+    {
+        // Check if the game needs to be paused
+        PauseGame();
+    }
+
+    private void PauseGame()
+    {
+        // If esc is pressed or the pause button is clicked,
+        if (Input.GetKeyDown(KeyCode.Escape) || pauseClicked == true)
+        {
+            // If game is not currently paused
+            if (gamePaused == false)
+            {
+                // Pause the game
+                Time.timeScale = 0;
+
+                // Game becomes paused
+                gamePaused = true;
+            }
+            // If game is currently paused
+            else
+            {
+                // Unpause the game
+                Time.timeScale = 1;
+
+                // Game becomes unpaused
+                gamePaused = false;
+            }
+        }
+    }
+
+    public void PauseButtonDown()
+    {
+        // When the pause button is clicked, the game is paused
+        pauseClicked = true;
+    }
+
+    public void PauseButtonUp()
+    {
+        // Tells PauseGame() that the pause button is no longer being clicked
+        pauseClicked = false;
+    }
+}
---
+++ TO-Jam-2021-Game/Assets/Scripts/UI/VolumeSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    // Text to change volume %
+    [SerializeField] Text VolText;
+
+    // Volume % control
+    public void Volume(float sliderValue)
+    {
+        // Convert slider value to appropriate output (from 0-2% to 0-100%)
+        sliderValue *= 100 / 2;
+        sliderValue = Mathf.Round(sliderValue);
+
+        // Update text
+        VolText.text = sliderValue.ToString() + "%";
+
+    }
+}
