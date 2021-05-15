30ef07661b2e99b29a3a052affacd782f8e649e4
Zoeghs <pilgrimz@sheridancollege.ca>
2021-05-15 19:21:37 -0400

    Completed rough pause menu *requires double click tho*
    - Pause menu is set up in scene "Zoe".
    - Problem where the menu needs to be double clicked to activate/deactivate after the first time. :/

--- TO-Jam-2021-Game/Assets/Scripts/UI/PauseMenu.cs
+++ TO-Jam-2021-Game/Assets/Scripts/UI/PauseMenu.cs
@@ -4,53 +4,57 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Var for pausing via button click
-    private bool pauseClicked = false;
-
     // Bool for pausing / unpausing
     private bool gamePaused = false;
 
+    // Access to canvases
+    [SerializeField] GameObject gameCanvas;
+    [SerializeField] GameObject pauseCanvas;
+    [SerializeField] GameObject controlsCanvas;
+    [SerializeField] GameObject settingsCanvas;
+    [SerializeField] GameObject creditsCanvas;
+
     private void Update()
     {
-        // Check if the game needs to be paused
-        PauseGame();
-    }
-
-    private void PauseGame()
-    {
-        // If esc is pressed or the pause button is clicked,
-        if (Input.GetKeyDown(KeyCode.Escape) || pauseClicked == true)
+        // Check if the game needs to be paused via esc key
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // If game is not currently paused
-            if (gamePaused == false)
-            {
-                // Pause the game
-                Time.timeScale = 0;
-
-                // Game becomes paused
-                gamePaused = true;
-            }
-            // If game is currently paused
-            else
-            {
-                // Unpause the game
-                Time.timeScale = 1;
-
-                // Game becomes unpaused
-                gamePaused = false;
-            }
+            PauseGame();
         }
     }
 
-    public void PauseButtonDown()
+    public void PauseGame()
     {
-        // When the pause button is clicked, the game is paused
-        pauseClicked = true;
-    }
+        // If game is not currently paused
+        if (gamePaused == false)
+        {
+            // Pause the game
+            Time.timeScale = 0;
 
-    public void PauseButtonUp()
-    {
-        // Tells PauseGame() that the pause button is no longer being clicked
-        pauseClicked = false;
+            // Game becomes paused
+            gamePaused = true;
+
+            // Enable / disable canvases
+            pauseCanvas.SetActive(true);
+            gameCanvas.SetActive(false);
+        }
+        // If game is currently paused
+        else
+        {
+            // Unpause the game
+            Time.timeScale = 1;
+
+            // Game becomes unpaused
+            gamePaused = false;
+
+            // Enable / disable canvases
+            gameCanvas.SetActive(true);
+            pauseCanvas.SetActive(false);
+            controlsCanvas.SetActive(false);
+            settingsCanvas.SetActive(false);
+            creditsCanvas.SetActive(false);
+        }
+
+        print(gamePaused);
     }
 }
