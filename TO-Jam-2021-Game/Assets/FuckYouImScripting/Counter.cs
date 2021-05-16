0d8bbb243f2f1756881ca3cafd384640d587ca60
GDS-Sheridan <71096848+GDS-Sheridan@users.noreply.github.com>
2021-05-16 08:00:53 -0400

    Added Audio to majority of sounds, things remaining are VFX, Environment and UI

--- TO-Jam-2021-Game/Assets/FuckYouImScripting/Counter.cs
+++ TO-Jam-2021-Game/Assets/FuckYouImScripting/Counter.cs
@@ -14,14 +14,47 @@ public class Counter : MonoBehaviour
 
     public Slider counterSlider;
 
+    private FMOD.Studio.EventInstance collectable;
+
+    [SerializeField]
+    int comboCount;
+
+    [SerializeField]
+    float comboMaxTime;
+
+    [SerializeField]
+    float comboTimeRemaning;
+
     void Start()
     {
-        
+
+    }
+
+    private void Update()
+    {
+
+        comboTimeRemaning += Time.deltaTime;
+
+        if (comboTimeRemaning >= comboMaxTime)
+        {
+            comboCount = 0;
+        }
+
     }
 
 
+    void ResetComboTime()
+    {
+        comboTimeRemaning = 0;
+    }
+
     public void addCount(bool white)
     {
+
+        PlayCombo();
+        comboCount += 1;
+        ResetComboTime();
+
         if (white)
         {
             totalWhite++;
@@ -35,5 +68,18 @@ public class Counter : MonoBehaviour
 
         counterSlider.value = totalWhite / (totalWhite + totalBlack);
 
+    }
+
+    public void PlayCombo()
+    {
+        collectable = FMODUnity.RuntimeManager.CreateInstance("event:/Collectable");
+        collectable.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+
+        collectable.setParameterByName("CollectCombo", comboCount);
+        
+
+
+        collectable.start();
+        collectable.release();
     }
 }
